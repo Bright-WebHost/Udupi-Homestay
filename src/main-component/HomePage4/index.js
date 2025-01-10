c27348019bb620df44500401d93771834bf8521e
ismail-is <ismailthalithanooji@gmail.com>
2025-01-10 16:36:50 +0530

    push 6

--- src/main-component/HomePage4/index.js
+++ src/main-component/HomePage4/index.js
@@ -25,7 +25,7 @@ const HomePage4 =() => {
            
             <Hero4/>
            
-            <Booking/>
+            {/* <Booking/> */}
             <About4/>
             <BlogSection/>
                  {/* <BlogSectionS2/> */}
